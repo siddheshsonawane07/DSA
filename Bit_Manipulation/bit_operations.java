b8be91bd66894878d30f32880b68104dd3d8ab60
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-01-23 00:42:10 +0530

    bit operations

--- Bit_Manipulation/bit_operations.java
+++ Bit_Manipulation/bit_operations.java
@@ -1,26 +1,65 @@
 package Bit_Manipulation;
 
 public class bit_operations {
-    public static void main(String[] args) {
-        //Get Bit
-        /*
-        Get the 3rd bit(position = 2) of number n = 0101
-        1) Bit mask: 1 << i = 1 << 2 = 0001 << 2 = 0100
-        bit mask number: 0100
 
-        2) Operation: AND
-        n AND bit mask
-        0101 AND 0100 => 0100
-        if it is a non-zero number then it contains 1
-        */
+    static void getBit(int n, int k){
+        //operation: bitmask AND number
+         int bitmask = 1 << k;
 
+        if((bitmask & n) == 0){
+            System.out.println("bit is zero");
+        }
+        else{
+            System.out.println("bit is one");
+        }
+    }
+
+    static int setKthBit(int n, int k)
+    {
         //Set Bit
+        //operation: bitmask OR number
+        //Setting a bit means that if K-th bit is 0, then set it to 1 and if it is 1 then leave it unchanged.
+        // kth bit of n is being set by this operation
+        int bitmask = 1 << k;
+        return (bitmask | n);
+    }
 
-
+    static int clearBit(int n, int k)
+    {
         //Clear Bit
+        //operation: number AND NOT bitmask
+        //Clearing a bit means that if K-th bit is 1, then clear it to 0 and if it is 0 then leave it unchanged.
+        // Function to clear the kth bit of n
+        int bitmask = 1 << k;
+        int negativebitmask = ~(bitmask);
+        return (n & negativebitmask);
+    }
 
+    static int toggleBit(int oper, int n, int k)
+    {
+        //oper = 1 for setting the value to 1
+        //oper = 0 for clearing the value to 0
+        //also called as updating bit
+        // Toggling a bit means that if K-th bit is 1, then change it to 0 and if it is 0 then change it to 1.
+        // Function to toggle the kth bit of n
+        //return (n ^ (1 << (k - 1)));
 
-        //Update Bit
+        int bitmask = 1 <<k;
+        if(oper == 1){
+            //setting the bit
+            return (bitmask | n);
+        }
+        else if(oper == 0) {
+            int negativebitmask = ~(bitmask);
+            return (n & negativebitmask);
+        }
+        else{
+            return -1;
+    }
+    }
+
+    public static void main(String[] args) {
+
 
     }
 }
