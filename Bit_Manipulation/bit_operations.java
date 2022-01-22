9a32dc69486ef39bcbb0467399a2b208010dcd51
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-01-22 18:24:29 +0530

    bitwise operators

---
+++ Bit_Manipulation/bit_operations.java
@@ -0,0 +1,27 @@
+package Bit_Manipulation;
+
+public class bit_operations {
+    public static void main(String[] args) {
+
+        //Get Bit
+        /*
+        Get the 3rd bit(position = 2) of number n = 0101
+        1) Bit mask: 1 << i = 1 << 2 = 0001 << 2 = 0100
+        bit mask number: 0100
+
+        2) Operation: AND
+        n AND bit mask
+        0101 AND 0100 => 0100
+        if it is a non-zero number then it contains 1
+        */
+
+        //Set Bit
+
+
+        //Clear Bit
+
+
+        //Update Bit
+
+    }
+}
