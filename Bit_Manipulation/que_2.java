735f8908c3027c0e914564e632c7faa7aa31f7e6
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-01-24 13:27:21 +0530

    que 2

---
+++ Bit_Manipulation/que_2.java
@@ -0,0 +1,34 @@
+package Bit_Manipulation;
+
+/*
+1) 5 ^ 5 = 0
+2) 0 ^ n = n
+
+Find the only non-repeating element in an array where every element repeats twice
+
+a = [5,4,1,4,3,5,1]
+Dry run
+int res = 0
+res = 0 ^ 5
+res = 5 ^ 4 ^ 1 ^ 4
+res = 5 ^ 1 ^ 3
+res = 5 ^ 1 ^ 3 ^ 5
+res = 1 ^ 3 ^ 1
+res = 3
+
+*/
+
+import java.util.Scanner;
+
+public class que_2 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int[] a = {5,4,4,1,3,5,1};
+        int result = 0;
+        for (int i = 0; i < a.length; i++) {
+            result ^= a[i];
+        }
+        System.out.println(result);
+    }
+}
