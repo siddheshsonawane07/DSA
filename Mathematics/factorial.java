6c03148507ca2f04d6e18644162f59e79a0d419c
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-01-27 23:14:19 +0530

    changes

---
+++ Mathematics/factorial.java
@@ -0,0 +1,30 @@
+package Mathematics;
+
+import java.util.Scanner;
+
+public class factorial {
+
+    static int factorial1(int n){
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            return (n * factorial1(n - 1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int number = in.nextInt();
+        int fact = 1;
+
+        for(int i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+
+        int number1= in.nextInt();
+        int fact1 = factorial1(number1);
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}
