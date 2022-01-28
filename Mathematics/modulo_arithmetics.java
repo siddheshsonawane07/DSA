0cf891261fc709f5224391e2ca4d39850fa779eb
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-01-29 00:11:25 +0530

    badme karenge

---
+++ Mathematics/modulo_arithmetics.java
@@ -0,0 +1,16 @@
+package Mathematics;
+
+public class modulo_arithmetics {
+
+    static int fastPower(int a, int b){
+        int res = 1;
+
+    return res;
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
