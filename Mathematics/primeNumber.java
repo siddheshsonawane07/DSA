15a1c35ef7db0115919a84ab31d1cefea1bad6b4
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-01-28 19:38:51 +0530

    prime number

--- Mathematics/primeNumber.java
+++ Mathematics/primeNumber.java
@@ -1,29 +1,39 @@
 package Mathematics;
 import java.util.*;
 
+/*Given a number n, print all primes smaller than or equal to n. It is also given that n is a small number.
+https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+ */
 
 
 public class primeNumber {
     public static void main(String[] args) {
-        boolean isPrime[] = seiveofEratoSthenes(10);
-        for (int i = 0; i < isPrime.length; i++) {
-            System.out.println(i+"is prime number: "+ isPrime[i]);
-        }
-
+        seiveofEratoSthenes(20);
     }
 
-    static boolean[] seiveofEratoSthenes(int n){
-        boolean isPrime[] = new boolean[n+1];
+    static void seiveofEratoSthenes(int n) {
 
-        Arrays.fill(isPrime,true);
+        {
+            // Create a boolean array and initialising its value as true, the value will be false if the value is not a prime number
+            boolean[] prime = new boolean[n + 1];
+            for (int i = 0; i <= n; i++)
+                prime[i] = true;
 
-        for (int i = 2; i * i <=n ; i++) {
-            for (int j = 0; j < 2 * i; j+=i) {
-                isPrime[j] = false;
+            for (int p = 2; p * p <= n; p++) {
+                // If prime[p] is not changed, then it is a
+                // prime
+                if (prime[p] == true) {
+                    // Update all multiples of p
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+                }
+            }
+
+            // Print all prime numbers
+            for (int i = 2; i <= n; i++) {
+                if (prime[i] == true)
+                    System.out.print(i + " ");
             }
         }
-
-
-        return isPrime;
     }
 }
