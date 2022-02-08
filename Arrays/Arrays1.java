3e2ebecf3831f41a55fcf5fe3eec16880811bd7b
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-02-08 18:00:42 +0530

    changes

--- Arrays/Arrays1.java
+++ Arrays/Arrays1.java
@@ -1,8 +1,5 @@
 package Arrays;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Arrays1 {
     public static void main(String[] args) {
         //DSA ARRAYS
---
+++ backtracking/nqueens.java
@@ -0,0 +1,7 @@
+package backtracking;
+
+public class nqueens {
+    public static void main(String[] args) {
+
+    }
+}
