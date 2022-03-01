f4488f554b49eb31417bbda6860cce8d216bf62b
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-01 13:59:41 +0530

    stack

--- Stacks/StackUsingFramework.java
+++ Stacks/StackUsingFramework.java
@@ -17,13 +17,6 @@ public class StackUsingFramework {
 
     public static void main(String[] args) {
         Stack<Integer> s1 = new Stack<>();
-        s1.push(5);
-        s1.push(4);
-        s1.push(3);
-        s1.push(2);
-        s1.push(1);
-
-        pushAtBottom(10,s1);
 
         while(!s1.isEmpty()){
             System.out.println(s1.peek());
