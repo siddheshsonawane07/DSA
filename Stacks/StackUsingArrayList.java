a6426333ccd20d336513e95f7b42a8d05457ec8f
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-01 13:55:58 +0530

    stack

--- Stacks/StackUsingArrayList.java
+++ Stacks/StackUsingArrayList.java
@@ -31,11 +31,6 @@ public class StackUsingArrayList {
 
     public static void main(String[] args) {
         Stack s1 = new Stack();
-        s1.push(5);
-        s1.push(4);
-        s1.push(3);
-        s1.push(2);
-        s1.push(1);
 
         while(!s1.isEmpty()){
             System.out.println(s1.peek());
