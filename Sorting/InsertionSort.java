482f54c2b67a260a6302f3df9d90e46dfa8d8625
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-04 00:17:15 +0530

    sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -2,6 +2,21 @@ package Sorting;
 
 import java.util.Arrays;
 
+/*
+The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+
+12, 11, 13, 5, 6
+Let us loop for i = 1 (second element of the array) to 4 (last element of the array)
+i = 1. Since 11 is smaller than 12, move 12 and insert 11 before 12
+11, 12, 13, 5, 6
+i = 2. 13 will remain at its position as all elements in A[0..I-1] are smaller than 13
+11, 12, 13, 5, 6
+i = 3. 5 will move to the beginning and all other elements from 11 to 13 will move one position ahead of their current position.
+5, 11, 12, 13, 6
+i = 4. 6 will move to position after 5, and elements from 11 to 13 will move one position ahead of their current position.
+5, 6, 11, 12, 13
+*/
+
 public class InsertionSort {
     public static void main(String[] args) {
         int [] arr = {8,7,6,4,5,1,0,2};
@@ -15,7 +30,6 @@ public class InsertionSort {
         for (int i = 1; i < n; i++) {
             int key = array[i];
             int j = i - 1;
-
 
             while (j>=0 && array[j]>key){
                 array[j + 1] = array[j];
