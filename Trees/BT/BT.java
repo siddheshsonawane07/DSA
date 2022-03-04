fab376426ac9009350d6aa7c0a988b28e4dd5aa3
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-04 23:00:47 +0530

    trees

--- Trees/BT/BT.java
+++ Trees/BT/BT.java
@@ -32,16 +32,52 @@ public class BT {
 
         root = new Node(data);
 
-        System.out.println("enter left for"+ data);
+        System.out.println("enter left for  "  + data);
         root.left = createTree();
 
-        System.out.println("enter right for"+ data);
+        System.out.println("enter right for  "  + data);
         root.right = createTree();
 
         return root;
     }
 
+    static void inOrder(Node root){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    static void preOrder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    static void postOrder(Node root){
+        if(root==null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + "");
+    }
+
     public static void main(String[] args) {
 
+        sc = new Scanner(System.in);
+
+        Node root = createTree();
+        System.out.println("inorder: ");
+        inOrder(root);
+        System.out.println("preorder: ");
+        preOrder(root);
+        System.out.println("postorder: ");
+        postOrder(root);
     }
 }
