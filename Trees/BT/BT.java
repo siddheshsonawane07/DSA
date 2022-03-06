45d8b778045971ae121dcb9571aef869ffdf4111
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-06 23:04:24 +0530

    trees

--- Trees/BT/BT.java
+++ Trees/BT/BT.java
@@ -156,34 +156,19 @@ public class BT {
 
     }
 
-    static void levelorder(Node root) {
-        if (root == null) {
+    static int max = 0;
+
+    static void leftview(Node node, int level) {
+        if (node == null)
             return;
+
+        if (max < level) {
+            System.out.print(" " + node.data);
+            max = level;
         }
 
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        q.add(null);
-
-        while (!q.isEmpty()) {
-            Node currNode = q.remove();
-            if (currNode == null) {
-                System.out.println();
-                if (q.isEmpty()) {
-                    break;
-                } else {
-                    q.add(null);
-                }
-            } else {
-                System.out.print(currNode.data + " ");
-                if (currNode.left != null) {
-                    q.add(currNode.left);
-                }
-                if (currNode.right != null) {
-                    q.add(currNode.right);
-                }
-            }
-        }
+        leftview(node.left, level + 1);
+        leftview(node.right, level + 1);
     }
 
 
