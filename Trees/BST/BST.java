e8c48379b8fbe6083a50bffec4db52f8ceb3a0ec
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-11 16:02:18 +0530

    trees

--- Trees/BST/BST.java
+++ Trees/BST/BST.java
@@ -11,76 +11,20 @@ public class BST {
 
     }
 
-    public Node insert(Node root, int data) {
-        if(root == null) {
-            return new Node(data);
-        }
-        else if (data <= root.data) {
-            root.left = insert(root.left, data);
-        }
-        else {
-            root.right = insert(root.right, data);
-        }
-        return root;
-    }
-
-    public boolean search(Node root, int data) {
-        if(root == null) {
+    static boolean search(Node root, int data) {
+        if (root == null) {
             return false;
-        }
-        else if (root.data == data) {
+        } else if (root.data == data) {
             return true;
-        }
-        else if (data <= root.data) {
+        } else if (data <= root.data) {
             return search(root.left, data);
-        }
-        else {
+        } else {
             return search(root.right, data);
         }
     }
 
-    public int findMin(Node root) {
-        while (root.left != null) {
-            root = root.left;
-        }
-        return root.data;
-
-    }
-
-    public int findMax(Node root){
-        while (root.right != null){
-            root = root.right;
-        }
-        return root.data;
-    }
-
-    public Node delete(Node root, int data) {
-        if(root == null) {
-            return null;
-        }
-        else if (data < root.data) {
-            root.left = delete(root.left, data);
-        }
-        else if (data > root.data) {
-            root.right = delete(root.right, data);
-        }
-        else {
-            if(root.left == null && root.right == null) {
-                root = null;
-            }
-            else if (root.left == null) {
-                root = root.right;
-            }
-            else if (root.right == null) {
-                root = root.left;
-
-            }
-            else {
-                root.data = findMin(root.right);
-
-            }
-        }
-        return root;
+    static int ceil(Node root, int key) {
+        return 0;
     }
 
     public static void main(String[] args) {
