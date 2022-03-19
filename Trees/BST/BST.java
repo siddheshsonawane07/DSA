e8335fb8fcf4b0ac77b81fb978ad457422fe2a20
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-19 19:03:10 +0530

    trees

--- Trees/BST/BST.java
+++ Trees/BST/BST.java
@@ -1,9 +1,9 @@
 package Trees.BST;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Scanner;
 
 public class BST {
+
     static class Node {
         int data;
         Node left;
@@ -12,115 +12,101 @@ public class BST {
         Node(int data) {
             this.data = data;
         }
+
     }
 
-    static void insert(Node node, int data) {
-        if (node == null) {
-            System.out.println("Node cannot be null");
-        }
-        Node cur = node;
-        while (true) {
-            if (cur.data <= data) {
-                if (cur.right != null) {
-                    cur = cur.right;
-                } else {
-                    cur.right = new Node(data);
-                    break;
-                }
-            } else {
-                if (cur.left != null) {
-                    cur = cur.left;
-                } else {
-                    cur.left = new Node(data);
-                    break;
-                }
-            }
-        }
-    }
-
-    static boolean search(Node node, int data) {
-        if (node == null) {
-            return false;
-        } else if (node.data == data) {
-            return true;
-        } else if (data <= node.data) {
-            return search(node.left, data);
+    public Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else if (data <= root.data) {
+            root.left = insert(root.left, data);
         } else {
-            return search(node.right, data);
+            root.right = insert(root.right, data);
         }
+        return root;
     }
 
-    //Node with the smallest data larger than or equal to the key value.
-    static int ceil(Node node, int key) {
-        int ceil = -1;
-        while (node != null) {
-            if (node.data == key) {
-                ceil = node.data;
-                return ceil;
-            }
-            if (key > node.data) {
-                node = node.right;
-            } else {
-                ceil = node.data;
-                node = node.left;
-            }
-        }
-        return ceil;
-    }
-
-    //Node with the greatest data lesser than or equal to the key value.
-    static int floor(Node node, int key) {
-        int floor = -1;
-        while (node != null) {
-            if (node.data == key) {
-                floor = node.data;
-                return floor;
-            }
-            if (key > node.data) {
-                floor = node.data;
-                node = node.right;
-            } else {
-                node = node.left;
-            }
-        }
-        return floor;
-    }
-
-    static int height(Node root) {
+    public boolean search(Node root, int data) {
         if (root == null) {
-            return 0;
+            return false;
+        } else if (root.data == data) {
+            return true;
+        } else if (data <= root.data) {
+            return search(root.left, data);
+        } else {
+            return search(root.right, data);
         }
-        return Math.max(height(root.left), height(root.right));
     }
 
-    static void level_order_traversal(Node root) {
+    public int findMin(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root.data;
+
+    }
+
+    public int findMax(Node root) {
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root.data;
+    }
+
+    public Node delete(Node root, int data) {
         if (root == null) {
-            System.out.println("Tree is empty");
-        }
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.add(root);
+            return null;
+        } else if (data < root.data) {
+            root.left = delete(root.left, data);
+        } else if (data > root.data) {
+            root.right = delete(root.right, data);
+        } else {
+            if (root.left == null && root.right == null) {
+                root = null;
+            } else if (root.left == null) {
+                root = root.right;
+            } else if (root.right == null) {
+                root = root.left;
 
-        //remove print add
-        while (queue.size() > 0) {
-            int count = queue.size();
-            for (int i = 0; i < count; i++) {
-                root = queue.remove();
-                System.out.println(root.data + " ");
-            }
-            if (root.left != null) {
-                queue.add(root.left);
-            }
+            } else {
+                root.data = findMin(root.right);
 
-            if (root.right != null) {
-                queue.add(root.right);
             }
         }
-        System.out.println();
+        return root;
     }
+
 
     public static void main(String[] args) {
-        Node root = new Node(1);
-//
-        //
+        BST tree = new BST();
+        Node root = null;
+        root = tree.insert(root, 15);
+        root = tree.insert(root, 10);
+        root = tree.insert(root, 20);
+        root = tree.insert(root, 25);
+        root = tree.insert(root, 8);
+        root = tree.insert(root, 12);
+
+        root = tree.delete(root, 10);
+
+
+        int num;
+        System.out.println("Enter the number to search");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+
+        if (tree.search(root, num)) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+
+
+        System.out.println("Min element is " + tree.findMin(root));
+
+        System.out.println("Max element is " + tree.findMax(root));
+
+
     }
+
 }
