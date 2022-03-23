5d4cde044db3a0f7182b67ed7e6ddc1eb1a0b8c3
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-23 10:19:57 +0530

    hashing

--- Hashing/HashTable.java
+++ Hashing/HashTable.java
@@ -1,49 +1,8 @@
 package Hashing;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
-
 public class HashTable {
-    //synchronized implementation of hashing
-
-    static void HashTableSync() {
-        Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
-        ht.put(1, "Sid");
-        ht.put(2, "Rush");
-        ht.put(3, "Tan");
-
-        System.out.println(ht);
-    }
-
-//    static void HashMapAsync(int[] arr) {
-//        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-//        for (int j : arr) {
-//            Integer c = hm.get(j);
-//            //if this is the first occurrence of the element
-//            if (hm.get(j) == null) {
-//                hm.put(j, 1);
-//            } else {
-//                hm.put(j, ++c);
-//            }
-//        }
-//        System.out.println(hm);
-//    }
-
-    static void BasicLinkedHashMap() {
-        LinkedHashMap<String, String> lhm = new LinkedHashMap<String, String>();
-        lhm.put("one", "first");
-        lhm.put("two", "second");
-        lhm.put("three", "third");
-
-        System.out.println(lhm);
-    }
 
     public static void main(String[] args) {
 
-        HashTableSync();
-//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-//        HashMapAsync(arr);
-        BasicLinkedHashMap();
     }
 }
