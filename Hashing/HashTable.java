aa7e4be56ec6b79ccb32800769ce212cb6437859
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-23 09:46:38 +0530

    hashing

--- Hashing/HashTable.java
+++ Hashing/HashTable.java
@@ -1,7 +1,27 @@
 package Hashing;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class HashTable {
+    //synchronized implementation of hashing
+
+    static void HashTableSync() {
+        Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
+        ht.put(1, "Sid");
+        ht.put(2, "Rush");
+        ht.put(3, "Tan");
+
+        System.out.println(ht);
+    }
+
+    static void HashMapAsync() {
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+    }
+
     public static void main(String[] args) {
-        
+
+        HashTableSync();
     }
 }
