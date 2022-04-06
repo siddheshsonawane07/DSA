0cc9813ff76c478d751ca89f3c6f1fabeb98deae
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-06 19:24:36 +0530

    array deque

---
+++ Collections_Framework/ArrayDequess.java
@@ -0,0 +1,7 @@
+package Collections_Framework;
+
+public class ArrayDequess {
+    public static void main(String[] args) {
+        
+    }
+}
--- Collections_Framework/priorityqueuess.java
+++ Collections_Framework/priorityqueuess.java
@@ -1,4 +1,30 @@
 package Collections_Framework;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class priorityqueuess {
+
+    public static void main(String[] args) {
+
+        //implements a min heap data structure
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        //implements a max heap data structure
+        //PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.offer(4);
+        pq.offer(3);
+        pq.offer(2);
+        pq.offer(6);
+
+        System.out.println(pq);
+
+        //removes the smallest number and changes the tree order accordingly
+        pq.poll();
+        System.out.println(pq);
+
+        //returns the element next in line
+        System.out.println(pq.peek());
+    }
 }
