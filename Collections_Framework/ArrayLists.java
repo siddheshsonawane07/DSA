c1fbd05ff61361208a6b812acda47a3f83e5f0a7
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-06 11:18:38 +0530

    arraylist

---
+++ Collections_Framework/ArrayLists.java
@@ -0,0 +1,18 @@
+package Collections_Framework;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+
+    //dynamic arrays
+    //array is copied into a new array with greater size
+    //if the initial size of the array is n, it increases the array size to n + (n/2) + 1
+
+    public static void main(String[] args) {
+
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Siddhesh");
+
+    }
+
+}
