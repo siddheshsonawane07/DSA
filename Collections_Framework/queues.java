10d8040eb3fda0802cbb61d91f0b720af423490f
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-06 19:12:56 +0530

    priority queues

--- Collections_Framework/queues.java
+++ Collections_Framework/queues.java
@@ -1,7 +1,25 @@
 package Collections_Framework;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+//first in first out
+
 public class queues {
     public static void main(String[] args) {
 
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+
+        System.out.println(queue);
+
+        //deletes and returns first value
+        System.out.println(queue.poll());
+
+        //returns the first value
+        System.out.println(queue.peek());
     }
 }
