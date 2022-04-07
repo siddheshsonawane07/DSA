286df9cdc34cd71d195865469c5b6f1495cd0e33
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-07 21:27:28 +0530

    hash maps

--- Collections_Framework/HashMapss.java
+++ Collections_Framework/HashMapss.java
@@ -1,4 +1,19 @@
 package Collections_Framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapss {
+
+    public static void main(String[] args) {
+
+        //stores data in key value pair
+        Map<Integer, String> num = new HashMap<Integer, String>();
+
+        num.put(1, "naruto");
+        num.put(2, "deathnote");
+
+        num.putIfAbsent(1, "Naruto Shippuden");
+        System.out.println(num);
+    }
 }
