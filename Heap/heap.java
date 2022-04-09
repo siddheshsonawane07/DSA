83551fd9c8175615d2063fa23790a61f25472cb5
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-09 20:14:15 +0530

    heap

--- Heap/heap.java
+++ Heap/heap.java
@@ -1,8 +1,10 @@
 package Heap;
 
+import java.util.ArrayList;
+
 public class heap {
-    public static void main(String[] args) {
-        /*
+
+    /*
         heap : a complete binary tree that satisfies heap's property
         max heap's property: the children nodes are smaller than root
         min heap's property: the children nodes are greater than root
@@ -13,6 +15,22 @@ public class heap {
         parent's index = (i/2)
         left child = 2 * i
         right child = 2 * i + 1
-        */
+        height of the tree is logn
+    */
+
+    void heapify(ArrayList<Integer> heap, int i) {
+        int size = heap.size();
+        int left = 2 * i;
+        int right = (2 * i) + 1;
+        int largest = i;
+
+        if (left < size && heap.get(left) > heap.get(right)) {
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
     }
 }
