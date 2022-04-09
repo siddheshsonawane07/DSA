8977b7611b63934b9091787439e44adade752807
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-09 16:31:00 +0530

    heap

--- Heap/heap.java
+++ Heap/heap.java
@@ -2,6 +2,17 @@ package Heap;
 
 public class heap {
     public static void main(String[] args) {
-        
+        /*
+        heap : a complete binary tree that satisfies heap's property
+        max heap's property: the children nodes are smaller than root
+        min heap's property: the children nodes are greater than root
+         in the last level of complete binary tree, preference must be given to left side
+
+        heap is created to represent it as array
+        if the current node's index is i, then to calculate:
+        parent's index = (i/2)
+        left child = 2 * i
+        right child = 2 * i + 1
+        */
     }
 }
