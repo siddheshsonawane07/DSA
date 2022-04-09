9cd1c89bf6d8cc7f08d88c7c71116727bd84c91a
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-09 21:55:17 +0530

    heapify

--- Heap/heap.java
+++ Heap/heap.java
@@ -18,19 +18,46 @@ public class heap {
         height of the tree is logn
     */
 
-    void heapify(ArrayList<Integer> heap, int i) {
-        int size = heap.size();
+    static void heapify(int[] heap, int i) {
+        int size = heap.length;
         int left = 2 * i;
         int right = (2 * i) + 1;
+
         int largest = i;
 
-        if (left < size && heap.get(left) > heap.get(right)) {
-
+        if (left < size && heap[left] > heap[largest]) {
+            largest = left;
         }
 
+        if (right < size && heap[right] > heap[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = heap[largest];
+            heap[largest] = heap[i];
+            heap[i] = heap[temp];
+
+            heapify(heap, i);
+        }
+        return;
+    }
+
+    static void print(int[] heap) {
+        for (Integer i : heap) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
+        int size = arr.length;
+
+        heapify(arr, size);
+        print(arr);
+
 
     }
 }
