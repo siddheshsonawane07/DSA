e53fb0f88c59eb104c0af324cdad4740a0e0253e
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-07-21 12:41:16 +0530

    matrrix

--- Arrays/matrix_multiplication.java
+++ Arrays/matrix_multiplication.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class matrix_multiplication {
 
@@ -8,20 +9,32 @@ public class matrix_multiplication {
 
         Scanner in = new Scanner(System.in);
 
-
         int[][] matrix = new int[row][col];
 
-
         for (int i = 0; i < row; i++) {
-
+            for (int j = 0; j < col; j++) {
+                System.out.println("Enter matrix for " + i + j);
+                matrix[i][j] = in.nextInt();
+            }
         }
 
         return matrix;
     }
 
-    static int[][] multiply(int[][] arr1, int[][] arr2) {
+    static int[][] multiply(int[][] arr1, int[][] arr2, int row1, int row2, int col1, int col2) {
 
-        int[][] result = new int[3][3];
+        int[][] result = new int[row1][col2];
+
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < col2; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < col1; k++) {
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
 
         return result;
     }
@@ -37,16 +50,18 @@ public class matrix_multiplication {
         System.out.println("Enter column size for matrix 1");
         int col1 = in.nextInt();
 
-        System.out.println("Enter row size for matrix 1");
+        System.out.println("Enter row size for matrix 2");
         int row2 = in.nextInt();
-        System.out.println("Enter column size for matrix 1");
+        System.out.println("Enter column size for matrix 2");
         int col2 = in.nextInt();
 
         int[][] matrix1 = matrix(row1, col1);
 
         int[][] matrix2 = matrix(row2, col2);
 
-        int[][] result = multiply(matrix1, matrix2);
+        int[][] result = multiply(matrix1, matrix2, row1, row2, col1, col2);
+
+        System.out.println(Arrays.toString(result));
 
 
     }
