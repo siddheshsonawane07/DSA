bec77c1c5ca2f79f7731f22c5ffd7bce6d307234
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-02-27 19:40:58 +0530

    improved sorting

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -19,7 +19,7 @@ i = 4. 6 will move to position after 5, and elements from 11 to 13 will move one
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int [] arr = {8,7,6,4,5,1,0,2};
+        int[] arr = {8, 7, 6, 4, 5};
         insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -28,14 +28,14 @@ public class InsertionSort {
         int n = array.length;
 
         for (int i = 1; i < n; i++) {
-            int key = array[i];
+            int current = array[i];
             int j = i - 1;
 
-            while (j>=0 && array[j]>key){
+            while (j >= 0 && array[j] > current) {
                 array[j + 1] = array[j];
                 j = j - 1;
             }
-         array[j+1]= key;
+            array[j + 1] = current;
         }
     }
 }
--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -4,27 +4,25 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        //searching for lowest number in the array and swapping it with the first element
-        //or searching for largest number in the array and swapping it with the last element
-        int [] arr = {8,7,6,4,5,1,0,2};
+        // searching for the smallest number in the array and swapping it with the first element
+        int[] arr = {8, 7, 6, 4, 5, 1, 0, 2};
         selection(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void selection(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
+    public static void selection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] > arr[index]){
-                    index = j;  //searching for lowest index
+            // searching for the smallest index (ascending order)
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
                 }
             }
+            // swapping the found smallest element with the current element at index i
             int temp = arr[index];
             arr[index] = arr[i];
             arr[i] = temp;
         }
     }
-
 }
-
