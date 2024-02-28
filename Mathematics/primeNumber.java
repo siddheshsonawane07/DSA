70ccb4ce8e8956bb08eae986ae0bc49aaf415ea4
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-02-28 20:25:36 +0530

    Update primeNumber.java

--- Mathematics/primeNumber.java
+++ Mathematics/primeNumber.java
@@ -7,33 +7,46 @@ https://www.geeksforgeeks.org/sieve-of-eratosthenes/
 
 
 public class primeNumber {
-    public static void main(String[] args) {
-        seiveofEratoSthenes(20);
-    }
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
 
-    static void seiveofEratoSthenes(int n) {
-
-        {
-            // Create a boolean array and initialising its value as true, the value will be false if the value is not a prime number
-            boolean[] prime = new boolean[n + 1];
-            for (int i = 0; i <= n; i++)
-                prime[i] = true;
-
-            for (int p = 2; p * p <= n; p++) {
-                // If prime[p] is not changed, then it is a
-                // prime
-                if (prime[p] == true) {
-                    // Update all multiples of p
-                    for (int i = p * p; i <= n; i += p)
-                        prime[i] = false;
-                }
-            }
-
-            // Print all prime numbers
-            for (int i = 2; i <= n; i++) {
-                if (prime[i] == true)
-                    System.out.print(i + " ");
+        for (int i = 2; i < n/2; i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
+        
+        return true;
     }
+
+    
+    public static void main(String[] args) {
+        boolean ans = isPrime(20);
+        System.out.println(ans);
+
+
+
+        static ArrayList<Integer> sieveOfEratosthenes(int N){
+        // code here
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        
+        for(int i = 0; i <= N; i++){
+            
+            boolean prime = isPrime(i);
+            
+            if(prime == true){
+              ans.add(i);
+            }
+            
+        }
+        
+        return ans;
+    }
+    }
+ 
 }
